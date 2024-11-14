1e833681e75911b1aff4f5c397c7ec550644d0f3
Elias Abueita <elabuait@umich.edu>
2024-11-14 16:44:42 -0500

    first attempt

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,9 @@
-// Code to implement the gcf function goes here
+int gcf(int &n1, int &n2) {
+    int remainder;
+    while ((n1%n2) > 0) {
+        remainder = n1%n2;
+        n1 = n2;
+        n2 = remainder;
+    }
+    return n2;
+}
