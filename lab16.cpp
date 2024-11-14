a0f2542e87aa6c29833579f9fcc185908225eaeb
Elias Abueita <elabuait@umich.edu>
2024-11-14 16:47:52 -0500

    second attempt

--- lab16.cpp
+++ lab16.cpp
@@ -1,7 +1,6 @@
 int gcf(int &n1, int &n2) {
-    int remainder;
     while ((n1%n2) > 0) {
-        remainder = n1%n2;
+        int remainder = n1%n2;
         n1 = n2;
         n2 = remainder;
     }
