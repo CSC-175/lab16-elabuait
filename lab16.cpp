0a5a5bd77d5c5dd0cde727564d1502720b6b320a
Elias Abueita <elabuait@umich.edu>
2024-11-14 17:34:50 -0500

    fifth attempt

--- lab16.cpp
+++ lab16.cpp
@@ -1,10 +1,9 @@
 int gcf(int &n1, int &n2) {
-
-    while (n2 != 0) {
-        int q = n1/n2;
-        int r = n1-q*n2;
-        n1 = n2;
-        n2 = r;
+int r;
+    while ((n1%n2)>0) {
+        r=n1%n2;
+        n1=n2;
+        n2=r;
     }
-    return n1;
+    return n2;
 }
